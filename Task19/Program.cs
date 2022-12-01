958830d303ef4b2ae34300e0c5b519d54628c30c
Chub <ychubykin@gmail.com>
2022-12-01 20:20:16 +0300

    Сделал первую версию ДЗ

---
+++ Task19/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+Console.Write("Введите пятизначное натуральное число: ");
+double n = Convert.ToDouble(Console.ReadLine());
+
+double n1 = Math.Floor(n / 10000);
+double n2 = Math.Floor((n / 1000) % 10);
+double n3 = Math.Floor((n / 10) % 10);
+double n4 = Math.Floor(n % 10);
+
+if (n1 == n4 && n2 == n3)
+    Console.WriteLine("Палиндром");
+else
+    Console.WriteLine("нет");
---
+++ Task21/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек
+//  и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+Console.Write("Введите координату X1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату X2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Z2: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+double result = 0;
+result = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)
+ + Math.Pow(z1 - z2, 2));
+Console.WriteLine(Math.Round (result, 2));
